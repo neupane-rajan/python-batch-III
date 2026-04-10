cd4cf50943d4ab46d8d1a290137f38286cc43c2a
Rajan <76466434+neupane-rajan@users.noreply.github.com>
2026-04-10 12:35:47 +0000

    for loop ended

---
+++ control-flow/loops/for_loop/01_loops.py
@@ -0,0 +1,26 @@
+#  a loop is used to repeat a block of code multiple times
+
+
+# for i in range(1000):
+#     print(i,"Hello world")
+
+
+# print number from 1,10
+# for i in range(1, 2):
+#     print(i)
+
+
+# there are mainly 2 types of loop in python:
+# 1. for loop-> used when we know how many times to repeat
+# 2. while loop -> used when we don't know exact count
+
+
+# enumerate
+
+# fruits = ["apple","banana","cherry"]
+fruits = "rajan"
+# enumerate le tesko item ani item ko index(position) ko value return garera dinxa
+for index in enumerate(fruits):
+    print(index)
+
+
